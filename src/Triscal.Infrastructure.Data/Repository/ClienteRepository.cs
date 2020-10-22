4770cb1a324d55de092c004bf92cdf0e0c3b4a96
charlesmendes13 <charlesluizmendes@gmail.com>
2020-10-22 00:05:50 -0300

    Adicionando Override em Metodos GetAll e GetById

--- src/Triscal.Infrastructure.Data/Repository/ClienteRepository.cs
+++ src/Triscal.Infrastructure.Data/Repository/ClienteRepository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Triscal.Domain.Entities;
 using Triscal.Domain.Interfaces.Repository;
 using Triscal.Infrastructure.Data.Context;
@@ -19,6 +22,55 @@ namespace Triscal.Infrastructure.Data.Repository
         {
             _factory = factory;
             _context = context;
+        }
+
+        public override async Task<IEnumerable<Cliente>> GetAllAsync()
+        {
+            try
+            {
+                var result = await _factory.DbConnection()
+                    .QueryAsync<Cliente, Endereco, Cliente>($"" +
+                    $"Select * From Cliente " +
+                    $"Left Join Endereco On Cliente.Id = Endereco.ClienteId " +
+                    $"", map: (cliente, endereco) =>
+                    {                       
+                        if (endereco != null)
+                            cliente.Endereco = endereco;
+
+                        return cliente;
+                    });
+
+                return result.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public override async Task<Cliente> GetByIdAsync(object id)
+        {
+            try
+            {
+                var result = await _factory.DbConnection()
+                    .QueryAsync<Cliente, Endereco, Cliente>($"" +
+                    $"Select * From Cliente " +
+                    $"Left Join Endereco On Cliente.Id = Endereco.ClienteId " +
+                    $"Where Cliente.Id = '{ id }'" +
+                    $"", map: (cliente, endereco) =>
+                    {
+                        if (endereco != null)
+                            cliente.Endereco = endereco;
+
+                        return cliente;
+                    });
+
+                return result.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
